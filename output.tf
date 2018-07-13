5284e63971bf4518f281958b07d7bd15d7e73e4b
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-13 14:54:21 +0300

    Migrate readme yaml

--- output.tf
+++ output.tf
@@ -1,19 +1,24 @@
 output "id" {
-  value = "${aws_elasticache_cluster.default.id}"
+  value       = "${aws_elasticache_cluster.default.id}"
+  description = "Disambiguated ID"
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.default.id}"
+  value       = "${aws_security_group.default.id}"
+  description = "Security group id"
 }
 
 output "port" {
-  value = "11211"
+  value       = "11211"
+  description = "Port"
 }
 
 output "config_host" {
-  value = "${module.dns_config.hostname}"
+  value       = "${module.dns_config.hostname}"
+  description = "Config host"
 }
 
 output "hosts" {
-  value = "${join(",", null_resource.host.*.triggers.name)}"
+  value       = "${join(",", null_resource.host.*.triggers.name)}"
+  description = "Hosts"
 }
--- variables.tf
+++ variables.tf
@@ -1,72 +1,89 @@
+variable "name" {
+  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
+}
+
 variable "namespace" {
-  default = "global"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
-  default = "default"
-}
-
-variable "name" {
-  default = "memcache"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "security_groups" {
-  type = "list"
+  type        = "list"
+  description = "AWS security group ids"
 }
 
 variable "vpc_id" {
-  default = ""
+  default     = ""
+  description = "VPC ID"
 }
 
 variable "max_item_size" {
-  default = "10485760"
+  default     = "10485760"
+  description = "Max item size"
 }
 
 variable "subnets" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
+  description = "AWS subnet ids"
 }
 
 variable "maintenance_window" {
-  default = "wed:03:00-wed:04:00"
+  default     = "wed:03:00-wed:04:00"
+  description = "Maintenance window"
 }
 
 variable "cluster_size" {
-  default = "1"
+  default     = "1"
+  description = "Cluster size"
 }
 
 variable "instance_type" {
-  default = "cache.t2.micro"
+  default     = "t2.micro"
+  description = "Elastic cache instance type"
 }
 
 variable "engine_version" {
-  default = "1.4.33"
+  default     = "1.4.33"
+  description = "Engine version"
 }
 
 variable "notification_topic_arn" {
-  default = ""
+  default     = ""
+  description = "Notification topic arn"
 }
 
 variable "alarm_cpu_threshold_percent" {
-  default = "75"
+  default     = "75"
+  description = "CPU threshold alarm level"
 }
 
 variable "alarm_memory_threshold_bytes" {
   # 10MB
-  default = "10000000"
+  default     = "10000000"
+  description = "Alarm memory threshold bytes"
 }
 
 variable "alarm_actions" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
+  description = "Alarm actions"
 }
 
 variable "apply_immediately" {
-  default = "true"
+  default     = "true"
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
 }
 
 variable "availability_zones" {
-  type = "list"
+  type        = "list"
+  description = "List of Availability Zones where subnets will be created"
 }
 
-variable "zone_id" {}
+variable "zone_id" {
+  default     = ""
+  description = "Route53 DNS Zone id"
+}

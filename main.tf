a6a6669844fe7ac339aad00c9eda88e4de74ea48
Siddarth Prakash <1428486+sidprak@users.noreply.github.com>
2021-06-07 18:57:48 -0400

    Revert "Add tags to param group"
    This reverts commit d9d4cf97eb73dd8209a6e44dc244da594a2f0255.

--- main.tf
+++ main.tf
@@ -82,7 +82,6 @@ resource "aws_elasticache_parameter_group" "default" {
   count  = var.enabled && !var.use_existing_parameter_group ? 1 : 0
   name   = module.label.id
   family = var.elasticache_parameter_group_family
-  tags   = module.label.tags
 
   parameter {
     name  = "max_item_size"

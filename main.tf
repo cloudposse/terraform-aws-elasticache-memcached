d66aba02c710819b3d4df5fb7466f1f13a5c3d77
Dan Meyers <danjbh@users.noreply.github.com>
2022-02-03 12:56:29 -0800

    Formatting updates

--- main.tf
+++ main.tf
@@ -95,19 +95,19 @@ resource "aws_elasticache_parameter_group" "default" {
 }
 
 resource "aws_elasticache_cluster" "default" {
-  count                        = local.enabled ? 1 : 0
-  apply_immediately            = var.apply_immediately
-  cluster_id                   = module.this.id
-  engine                       = "memcached"
-  engine_version               = var.engine_version
-  node_type                    = var.instance_type
-  num_cache_nodes              = var.cluster_size
-  parameter_group_name         = join("", aws_elasticache_parameter_group.default.*.name)
-  subnet_group_name            = local.elasticache_subnet_group_name
+  count                = local.enabled ? 1 : 0
+  apply_immediately    = var.apply_immediately
+  cluster_id           = module.this.id
+  engine               = "memcached"
+  engine_version       = var.engine_version
+  node_type            = var.instance_type
+  num_cache_nodes      = var.cluster_size
+  parameter_group_name = join("", aws_elasticache_parameter_group.default.*.name)
+  subnet_group_name    = local.elasticache_subnet_group_name
   # It would be nice to remove null or duplicate security group IDs, if there are any, using `compact`,
   # but that causes problems, and having duplicates does not seem to cause problems.
   # See https://github.com/hashicorp/terraform/issues/29799
-  security_group_ids         = local.create_security_group ? concat(local.associated_security_group_ids, [module.aws_security_group.id]) : local.associated_security_group_ids
+  security_group_ids           = local.create_security_group ? concat(local.associated_security_group_ids, [module.aws_security_group.id]) : local.associated_security_group_ids
   maintenance_window           = var.maintenance_window
   notification_topic_arn       = var.notification_topic_arn
   port                         = var.port

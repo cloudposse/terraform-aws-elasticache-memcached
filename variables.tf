b7b75ac5b3a1e0ea8fd06cf35b2e2ef47ef3e6e9
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-05-11 06:00:17 +0000

    Auto Format

--- variables.tf
+++ variables.tf
@@ -11,8 +11,8 @@ variable "parameter" {
   }))
   default = [
     {
-        name  = "max_item_size"
-        value = 10485760
+      name  = "max_item_size"
+      value = 10485760
     },
   ]
   description = "A list of Memcached parameters to apply. Note that parameters may differ from one Memcached family to another"
